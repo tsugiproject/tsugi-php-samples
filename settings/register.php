548a1ba1157e1113ab75aecbc852f25d7881832e
Charles Severance <csev@umich.edu>
2016-04-15 11:17:17 +0300

    Move more of the samples folder

---
+++ settings/register.php
@@ -0,0 +1,9 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Settings Unit Test Tool",
+"FontAwesome" => "fa-database",
+"short_name" => "Settings Test",
+"description" => "This tool allows you to exercise the LTI 2.x JSON Settings API."
+);
+
